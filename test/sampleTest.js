a82d6dee784e004200cc1346d61a57e704d544d7
webhdx <webhdx@gmail.com>
2016-09-21 11:58:43 +0200

    adds authentication

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,11 +1,21 @@
 var request = require('supertest');
 var assert = require('assert');
+var alawaysAuthenticatedAuthenticator = require('../lib/AlwaysAuthenticatedAuthenticator');
 
 describe('Stock inventory', function () {
+    it('checks login with invalid credentials', function (done) {
+        var repository = require('../lib/InMemoryRepository')();
+        var basicAuthAuthenticator = require('../lib/Authenticator');
+        var app = require('../lib/app')(repository, basicAuthAuthenticator);
+
+        request(app)
+            .get('/')
+            .expect(401, done);
+    });
     it('allows adding stock to inventory', function (done) {
         var repository = require('../lib/InMemoryRepository')();
         repository._items([{isbn: '1234', count: 1}]);
-        var app = require('../lib/app')(repository);
+        var app = require('../lib/app')(repository, alawaysAuthenticatedAuthenticator);
 
         request(app)
             .post('/stock')
@@ -20,7 +30,7 @@ describe('Stock inventory', function () {
     it('allows to check book availability', function (done) {
         var repository = require('../lib/InMemoryRepository')();
         repository._items([{isbn: '1234', count: 1}]);
-        var app = require('../lib/app')(repository);
+        var app = require('../lib/app')(repository, alawaysAuthenticatedAuthenticator);
 
         request(app)
             .get('/stock/1234')
